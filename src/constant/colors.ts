d13cd275c4a4f14a05d65a8c13d15befca140ce6
shahzodtoyirov99 <shahzodtoyirov99@gmail.com>
2022-12-20 16:48:16 +0500

    constanta files

---
+++ src/constant/colors.ts
@@ -0,0 +1,13 @@
+const Colors = {
+  white: '#fff',
+  mainBlack: '#1A0700',
+  mainColor: '#FF4201',
+  secondaryColor: '#FFE8E0',
+  red: '#FF0101',
+  gray: '#9D9693',
+  gray2: '#C9C5C4',
+  gray3: '#DFDFDF',
+  backColor: '#F5F5F5',
+};
+
+export default Colors;
